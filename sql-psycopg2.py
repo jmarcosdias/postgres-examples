bb93c25a42e7ffbed97bd2256fbbfb3952f07cca
jmarcosdias <jmsdias6@gmail.com>
2022-04-06 21:21:47 +0000

    Add sql-crud sql-orm files. Update previous py files

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -11,7 +11,7 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Artist"')
 
 # Query 2 - select only the "Name" column from the "Artist" table
-# cursor.execute('SELECT "Name" FROM "Artist"')
+cursor.execute('SELECT "Name" FROM "Artist"')
 
 # Query 3 - select only "Queen" from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
@@ -23,7 +23,7 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
